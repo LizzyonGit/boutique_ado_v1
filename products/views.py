050d902a521760b4073087f4098b0c4617fc28ab
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-05-23 11:25:14 +0200

    Add sorting

--- products/views.py
+++ products/views.py
@@ -10,10 +10,27 @@ def all_products(request):
     including sorting and search queries
     """
     products = Product.objects.all()
+    #  below variables are equal to none at the top. Since we'll need to make sure those are defined in order to return the template properly when we're not using any sorting etc.
     query = None
     categories = None
+    sort = None
+    direction = None
+    
 
     if request.GET:
+        if 'sort' in request.GET:
+                sortkey = request.GET['sort']
+                sort = sortkey
+                if sortkey == 'name':
+                    sortkey = 'lower_name'  # in the event the user is sorting by name
+                    products = products.annotate(lower_name=Lower('name'))
+                
+                if 'direction' in request.GET:
+                    direction = request.GET['direction']
+                    if direction == 'desc':
+                        sortkey = f'-{sortkey}'
+                products = products.order_by(sortkey)  # actually sorting the products
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -27,11 +44,14 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     } 
 
     return render(request, 'products/products.html', context)
