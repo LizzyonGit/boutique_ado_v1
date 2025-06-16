46158b471b5b167396c29e29315ad027b4a3e0f6
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-06-16 15:22:23 +0200

    Start webhook handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks
+    
+    The init method of the class is a setup method that's called every time an instance of the class is created.
+For us we're going to use it to assign the request as an attribute of the class
+just in case we need to access any attributes of the request coming from stripe.
+"""
+
+    def __init__(self, request):
+        self.request = request
+
+    
+    def handle_event(self, event):
+        """Handle generic/unknown/unexpected webhook event"""
+
+        return HttpResponse(
+            content=f'Webhook received: {event['type']}',
+            status=200)
+
