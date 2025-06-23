e828d3614d243b597077d34f931cdf9de4536f58
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-06-23 14:43:09 +0200

    Add edit functionality

--- products/views.py
+++ products/views.py
@@ -90,3 +90,28 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def edit_product(request, product_id):
+    """edit product"""
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully edited product')
+            return redirect(reverse('product_detail', args=[product.id]))  # redirect to product detail page using the product id
+        else:
+            messages.error(request, 'Failed to update product. Correct the invalid fields')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product
+    }
+
+    return render(request, template, context)
+
