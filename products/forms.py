fe9b426a3a26d114b6a6b16a56ce45fc65d77771
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-06-23 11:39:31 +0200

    Add form for store owners

---
+++ products/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Product, Category
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'  # all fields
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # override init method
+        categories = Category.objects.all()
+        """And create a list of tuples of the friendly names associated with their category ids.
+        This special syntax is called the list comprehension.
+        And is just a shorthand way of creating a for loop that adds items to a list.
+        get_friendly_name is method in Category model"""
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        
+        """Update the category field on the form to use those for choices instead of using the id."""
+        self.fields['category'].choices = friendly_names
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+        
+
